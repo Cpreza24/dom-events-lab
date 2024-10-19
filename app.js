2d4d73a4ebb1a8118fac636d6793731841d7df7a
Cpreza24 <cpreza93@yahoo.com>
2024-10-18 19:59:46 -0700

    added initial dom code

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 /*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll('.button');
+const calculator = document.querySelector('#calculator');
+const operator = '';
+const num1 = 0;
+const num2 = 0;
 
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
+buttons.forEach((button) => {
+    button.addEventListener('click', e => {
+        console.log(e.target.innerText);
+    })
+});
+
+calculator.addEventListener('click', e => {
+    console.log(e.target.innerText)
+})
 
 /*-------------------------------- Functions --------------------------------*/
